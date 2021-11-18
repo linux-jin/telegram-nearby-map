e9df949b7b5252424b1bd7025ce815b52139bc8b
雾山 <li997477295@outlook.com>
2021-11-18 10:13:55 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {}
+
+
+// your telegram API credentials
+// please go to https://my.telegram.org to create your API credentials
+config.telegramApiId = 3481183;
+config.telegramApiHash = '4ea7b352016e79f49f5a2be8656dd494';
+
+
+// server settings
+config.hostname = '127.0.0.1';
+config.port = 3000;
+
+
+module.exports = config;
